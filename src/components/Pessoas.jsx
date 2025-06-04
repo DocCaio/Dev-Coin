31c0fadc3562a140e35992f3e8e631deb1de6245
Caio Martins 😊 <108128119+DocCaio@users.noreply.github.com>
2025-06-04 15:05:14 -0300

    up

--- src/components/Pessoas.jsx
+++ src/components/Pessoas.jsx
@@ -1,8 +1,10 @@
+import styles from './Pessoas.module.css'
+
 function Pessoas({nome , idade , profissao, fotos}) {
     return (
         <div>
             <img src={fotos} alt={nome}/>
-            <h2>Nome: {nome}</h2>
+            <h2 className={styles.text}>Nome: {nome}</h2>
             <p>Idade: {idade}</p>
             <p>Profissao: {profissao}</p>
         </div>
