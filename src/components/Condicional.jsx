12a7cae4af933140dda14aefd5dbb7f0b9764e44
Caio Martins 😊 <108128119+DocCaio@users.noreply.github.com>
2025-06-11 10:05:43 -0300

    up

---
+++ src/components/Condicional.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function Condicional() {
+    return (
+        <div>
+            <h2>Cadastro o seu e-mail</h2>
+            <form>
+                <input type="email" placeholder="Digite o seu e-mail..."/>
+            </form>
+            <button onClick={}>Enviar e-mail</button>
+          
+        </div>
+    )
+}
+
+export default Condicional;
