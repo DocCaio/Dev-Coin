bec0cd9cc972df588a7caf53db5112f11744b487
Caio Martins 😊 <108128119+DocCaio@users.noreply.github.com>
2025-07-09 18:37:26 -0300

    up

--- src/components/pages/NewProjects.jsx
+++ src/components/pages/NewProjects.jsx
@@ -1,12 +1,35 @@
+import { data, useNavigate } from 'react-router';
 import { ProjectForm } from '../project/ProjectForm';
 import styles from './NewProjects.module.css';
 
+
 export const NewProject = () => {
+
+  const history = useNavigate();
+
+  function creatPost(project) {
+    project.cost = 0
+    project.services = []
+
+    fetch("http:/localhost;5000/projetcs" , {
+      method: "GET",
+      headers: {
+        "Content-Type": 'aplication/json',
+      },
+      body: JSON.stringify(project),
+    }).then((repo => repo.json())
+      .then((data) => {
+        console.log(data)
+      })    
+    ).catch(err => console.log(err))
+  }
+
+
   return (
     <div className={styles.newproject_container}>
       <h1>Criar projeto</h1>
       <p>Criar seu projeto para depois adcionar</p>
-      <ProjectForm btnText="Criar Projeto"/>
+      <ProjectForm handleSubmit={creatPost} btnText="Criar Projeto"/>
     </div>
   );
 }
--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -6,9 +6,10 @@ import styles from './ProjectForm.module.css';
 
 
 
-export const ProjectForm = ({btnText}) => {
+export const ProjectForm = ({handleSubmit ,  btnText ,projectData}) => {
 
     const [categories , setCategories] = useState([])
+    const [project , setProject ] = useState(projectData || {})
      
     useEffect(() => {
         
@@ -26,13 +27,34 @@ export const ProjectForm = ({btnText}) => {
     .catch((err) => console.log(err))
     })
 
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(project)
+    }
+
+
+    function handleChange(e) {
+        setProject({...project, [e.target.name]: e.target.value})
+        console.log(project)
+    }
+
+    function handleCategory(e) {
+        setProject({...project, category: {
+           id: e,target,value,
+           name: e.target.options[e.target.selectedIndex].text,
+        },          
+        })
+        console.log(project)
+    }
+
     return (
-        <form className={styles.form}>
+        <form onSubmitCapture={submit} className={styles.form}>
             <Input
             type="text" 
             text="Nome do projeto"
             name="name"
             placeholder="Insira o nome do projeto"
+            handleOnChange={handleChange}
             
             />
            <Input
