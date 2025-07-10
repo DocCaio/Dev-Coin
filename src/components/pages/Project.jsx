e558063ea3db4b472e4cb9fc2501e41ee59b0ecd
Caio Martins 😊 <108128119+DocCaio@users.noreply.github.com>
2025-07-10 18:50:13 -0300

    up

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,11 +1,18 @@
+import { Message } from '../layout/Message';
 import styles from './Project.module.css';
+import { useLocation } from 'react-router-dom';
 
 
 export const Project = () => {
+    const location = useLocation()
+    let message = ''
+    if (location.state) {
+        message = location.state.message
+    }
     return (
     <div className={styles.form_control}>
 
-              
+   {message &&  <Message type="sucess" msg="Alguma mensagem"/>  }
         
     </div>
     );
