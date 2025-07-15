8d2056e8d0fb80f29827c637d2c5789c319688c3
Caio Martins 😊 <108128119+DocCaio@users.noreply.github.com>
2025-07-15 10:21:38 -0300

    up

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,7 +1,27 @@
+import styles from './Projects.module.css';
+import { Container } from '../layout/Container';
+import { Message } from '../layout/Message';
+import { LinkButton } from '../layout/LinkButton';
+import { useLocation } from 'react-router';
+
 export const Projects = () => {
+  const location = useLocation();
+  let message = '';
+
+  if (location.state) {
+    message = location.state.message;
+  }
+
   return (
-    <div>
-      <h1>Projetos!</h1>
+    <div className={styles.projeto_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos!</h1>
+        <LinkButton to="/newprojects" text="Criar Projeto"/>
+        {message && <Message type="success" msg={message} />}
+      </div>
+      <Container customClass="start">
+        <p>Projetos...</p>        
+      </Container>
     </div>
   );
 }
