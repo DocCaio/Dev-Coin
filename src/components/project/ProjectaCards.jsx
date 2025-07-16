4cd14073eca6a789a137f061d80d6f3441e2ae71
Caio Martins 😊 <108128119+DocCaio@users.noreply.github.com>
2025-07-16 14:53:10 -0300

    up

---
+++ src/components/project/ProjectaCards.jsx
@@ -0,0 +1,15 @@
+
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs';
+import styles from './ProjectCards.module.css';
+
+
+export const NewProject = ({id , name , budget ,category, handleRemove}) => {
+  return (
+    <>
+    <p>Projeto</p>
+    </>
+  )
+
+}
+
+
